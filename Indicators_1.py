d22bb6f660f1f4c0187aa4d7f055726ea2b3102f
Bruce <mannypac12@gmail.com>
2018-08-21 17:05:14 +0900

    Initial commit test

--- Indicators_1.py
+++ Indicators_1.py
@@ -76,11 +76,7 @@ class tech_analysis_price:
 
         ## True: Up / False: Down
 
-        if self.cls_prc >= self.strt_prc:
-            return True
-
-        else:
-            return False
+        return self.cls_prc >= self.strt_prc
 
     def moving_avg(self, win, tpe = 'ema'):
 
@@ -91,6 +87,14 @@ class tech_analysis_price:
         elif tpe == 'ema':
 
             return self.cls_prc.ewm(span = win, min_periods = win).mean()
+
+    def price_mov(self, win = 5, tpe = 'ema'):
+
+        cond_1 = self.cls_prc > self.moving_avg(win=win, tpe=tpe)
+        cond_2 = self.candle_color()
+
+        return cond_1 & cond_2
+
 
     def mvg_cross_over(self, win1, win2, tpe = 'golden', mvg_type = 'ema'):
 
@@ -111,6 +115,37 @@ class tech_analysis_price:
             elif tpe == 'dead':
 
                 return (mvg_1 <= mvg_2) & (prv_mvg_1 >= prv_mvg_2)
+
+    ## Trend retracement Tools
+
+    def rete_move_1(self, win1 = 5, win2 = 20, tpe = 'golden', mvg_type = 'ema'):
+
+        """
+        If the 5 days meets 20 meets with Red candle
+
+        If the close price between 5 and 20, showing upward
+        :return:
+        """
+
+        cond1 = self.mvg_cross_over(win1=win1, win2=win2, tpe = tpe, mvg_type = mvg_type).shift(1)
+        cond2 = self.candle_color()
+
+        return cond1 & cond2
+
+    def value_zone(self, win1 = 5, win2 = 20):
+
+        """
+        :param win1: 20 day moving avg
+        :param win2: 5 day moving avg
+        :param win3: Candle should be +(red)
+        :return:
+        """
+
+        cond1 = self.strt_prc >= self.moving_avg(win = win2)
+        cond2 = self.cls_prc <= self.moving_avg(win = win1)
+        cond3 = self.candle_color()
+
+        return cond1 & cond2 & cond3
 
     def macd(self, win1 = 12, win2 = 26, sig = 9):
 
@@ -143,11 +178,54 @@ class tech_analysis_price:
 
             return (macd <= macd_sig) & (prev_macd >= prev_macd_signal)
 
+    ## Variables for money management
+
+    def atr(self, win = 14, sig = 9):
+
+        TR = self.high_prc.sub(self.low_prc.shift(1))
+        Prp_ATR = TR.ewm(span = win, min_periods = win).mean()
+        ## [Prior ATR * 13 + Current TR] / 14
+
+        ATR = (Prp_ATR.shift(1) * (win-1)).sub(-TR).div(win)
+        sig = ATR.ewm(span = sig, min_periods= sig).mean()
+
+        ans = pd.concat([ATR, sig], axis=1)
+        ans.columns = ['ATR', 'signal']
+
+        return ans.dropna()
 
 
+class money_management:
 
+    def __init__(self, capital):
 
+        self.cap = capital
 
+    def trade_risk(self, entry, stop):
+
+        return entry - stop
+
+    def fxed_dol_risk(self, entry, stop, unit):
+
+        trd_rsk = self.trade_risk(entry, stop)
+        fxd_rsk = self.cap / unit
+
+        return round(fxd_rsk / trd_rsk)
+
+    def fxed_cap_risk(self, max_dd, blw_t_risk):
+
+        ## Lag_dd = Dollar amount
+        ## blw_t_risk = acceptable risk you endure(perct)
+
+        fxd_cap = max_dd / blw_t_risk
+
+        return round(self.cap / fxd_cap)
+
+    def william_fxed(self, lag_loss, risk = 0.02):
+
+        dol_risk = self.cap * risk
+
+        return round(dol_risk / lag_loss)
 
 sql_kspi = """
 select trd_dt, strt_prc, high_prc, low_prc, cls_prc 
@@ -156,66 +234,10 @@ where trd_dt between '20150101' and '20170731' and
 u_cd in ('I.001')
 """
 
-a = data_provide('20180101', '20180530')
+a = data_provide('20100101', '20180530')
 test_dt = a.index_sql_collect(sql_kspi, "'I.201'")
-tech_analysis_price(test_dt).macd_cross_over(12, 26, 9, tpe = 'dead')
 
 
 
-
-sql_ksdq = """
-select trd_dt, strt_prc, high_prc, low_prc, cls_prc 
-from FNS_UD
-where trd_dt between '20150101' and '20170731' and 
-u_cd in ('I.201')
-"""
-
-def data_opener(sql, st_dt = '2015-01-01'):
-    
-    data = pd.read_sql(sql, conn, index_col = 'TRD_DT')
-    data.index = pd.to_datetime(data.index)
-
-    return data.loc[st_dt:]
-
-
-def mov_average(data, win = 5, price = 'CLS_PRC', met = 'SMA'):
-    
-    if met == 'SMA':
-        
-        mn_avg = data[price].rolling(window = win).mean()
-        
-    elif met == 'EMA':
-        
-        mn_avg = data[price].ewm(span = win, min_periods = win).mean()
-        
-    return mn_avg
-
-
-def DataGuideReader(data):
-    dt = pd.read_excel(data, skiprows = 9, 
-                       index_col = 10)
-
-    dt.drop(dt.index[0:4], inplace = True)
-    dt.columns[0] = 'TRD_DT'
-    
-    return dt
-
-## Bollinger Band
-
-def Bol_Band(data, win = 5, price = 'CLS_PRC', met = 'SMA'):
-    
-    dt = mov_average(data, win = win, price = price, met = met).rename('mv')
-    dt_ptstd = (dt + 2 * data[price].rolling(win).std()).rename('ptstd')
-    dt_mtstd = (dt - 2 * data[price].rolling(win).std()).rename('mtstd')
-    
-    return pd.concat([dt, dt_ptstd, dt_mtstd], axis = 1)
-
-## Effective Ratio
-    
-
-## Bollinger Band    
-
-kspi = data_opener(sql_kspi)
-ksdq = data_opener(sql_ksdq)
 
 
